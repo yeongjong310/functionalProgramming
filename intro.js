fa779c7d3ee84bfff07c23e632bb1d68a17dad66
yeongjong <tohero21@naver.com>
2022-06-02 21:22:19 +0900

    Add - pipe 함수

--- intro.js
+++ intro.js
@@ -2,6 +2,11 @@ const go = (init, ...callbacks) => {
   return callbacks.reduce((acc, callback) => callback(acc), init);
 };
 
+const pipe =
+  (...callbacks) =>
+  (init) =>
+    go(init, ...callbacks);
+
 const queryStr = (obj) => go(obj, Object.entries);
 const mul10 = (a) => a * 10;
 const add5 = (a) => a + 5;
@@ -9,3 +14,7 @@ const add5 = (a) => a + 5;
 console.log(queryStr({ limit: 10, offset: 10, type: "notice" }));
 
 console.log(go(5, add5, mul10)); // 100
+
+console.log(go(5, pipe(add5, mul10))); // 100
+
+console.log(pipe(add5, mul10)(5)); // 100
