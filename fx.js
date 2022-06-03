e8a3744510c6f9ef8674fdcd2203cd0820ffb6b7
yeongjong <tohero21@naver.com>
2022-06-03 16:06:01 +0900

    Add - take 함수 구현 및 L.range 효율성 테스트

--- fx.js
+++ fx.js
@@ -12,37 +12,37 @@ const curry =
   (arg, ...args) =>
     args.length > 0 ? f(arg, ...args) : (...args) => f(arg, ...args);
 
-const filter = curry((f, arr) => {
+const filter = curry((f, iter) => {
   const result = [];
 
-  for (const el of arr) {
+  for (const el of iter) {
     if (f(el)) result.push(el);
   }
 
   return result;
 });
 
-const map = curry((f, arr) => {
+const map = curry((f, iter) => {
   const result = [];
 
-  for (const el of arr) {
+  for (const el of iter) {
     result.push(f(el));
   }
 
   return result;
 });
 
-const reduce = curry((f, init, arr) => {
+const reduce = curry((f, init, iter) => {
   let acc;
 
-  if (!arr) {
-    arr = init[Symbol.iterator]();
-    acc = arr.next().value;
+  if (!iter) {
+    iter = init[Symbol.iterator]();
+    acc = iter.next().value;
   } else {
     acc = init;
   }
 
-  for (const el of arr) {
+  for (const el of iter) {
     acc = f(acc, el);
   }
 
@@ -61,4 +61,25 @@ const L = {
   },
 };
 
-console.log(...L.range(4));
+const take = (l, iter) => {
+  const result = [];
+
+  for (const el of iter) {
+    result.push(el);
+    if (result.length === l) break;
+  }
+
+  return result;
+};
+
+function test(name, time, f) {
+  console.time(name);
+  while (time--) f();
+  console.timeEnd(name);
+}
+
+// test("range", 10, () => reduce((a, b) => a + b), range(1000000));
+// test("L.range", 10, () => reduce((a, b) => a + b), L.range(1000000)); // L.range가 좀 더 빠르다.
+
+// test("range", 10, () => take(5, range(1000000)));
+// test("L.range", 10, () => take(5, L.range(1000000))); // 반복을 중간에 중단하는 경우 더 효율성이 뛰어나다.
